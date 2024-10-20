93e8d7c207344d6ec352323ac28ad4341324bdba
LittleWorm <n0bdy@tuta.io>
2024-10-20 08:45:05 +0200

    changes##Update dotnet-desktop.yml

---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using System.Diagnostics;
+
+namespace PhoneWord.Platforms.Android;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+public class MainActivity : MauiAppCompatActivity
+{
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
---
+++ Platforms/iOS/Program.cs
@@ -0,0 +1,18 @@
+﻿// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+using ObjCRuntime;
+using UIKit;
+
+namespace PhoneWord.Platforms.iOS;
+
+public class Program
+{
+    // This is the main entry point of the application.
+    static void Main(string[] args)
+    {
+        // if you want to use a different Application Delegate class from "AppDelegate"
+        // you can specify it here.
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
